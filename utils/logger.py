bb0d555d9ed821a0efe1b0550a8422322668700c
LCF-xybq <lcfxybq@163.com>
2022-04-23 16:04:23 +0800

    xtla

--- utils/logger.py
+++ utils/logger.py
@@ -16,7 +16,7 @@ def get_root_logger(log_file=None, log_level=logging.INFO):
     Returns:
         :obj:`logging.Logger`: The obtained logger
     """
-    logger = get_logger(name='TJU_VIPLab', log_file=log_file, log_level=log_level)
+    logger = get_logger(name='Handler One', log_file=log_file, log_level=log_level)
 
     return logger
 
